916b7b17a7d31386500184e24b336fd89cf7c14c
Daniël van Ginneken <dcj.vanginneken@student.avans.nl>
2024-10-08 17:25:45 +0200

    Front-end grind and Back-end preparation

--- L1Q1 Project Smart Energy/SmartEnergy.Library/Measurements/Models/Laadsessie.cs
+++ L1Q1 Project Smart Energy/SmartEnergy.Library/Measurements/Models/Laadsessie.cs
@@ -1,8 +1,7 @@
 namespace SmartEnergy.Library.Measurements.Models;
-
-public record Laadsessie(decimal prijsLaadsessie,
-                          int duurLaadsessie,
-                          double totaalkWhLaadsessie,
-                          string startLaadsessie,
-                          string stopLaadsessie);
-
+public record Laadsessie(DateTime Start,
+                          DateTime Stop,
+                          TimeSpan Duur,
+                          decimal Prijs,
+                          double TotaalKWh
+                          );
