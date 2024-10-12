e6e11a37c649ced031cf477c50b122565205cda0
Daniël van Ginneken <dcjvanginneken@gmail.com>
2024-10-12 17:55:32 +0200

    Finished Smart Energy project

--- L1Q1 Project Smart Energy/SmartEnergy.Library/Measurements/Models/Laadsessie.cs
+++ L1Q1 Project Smart Energy/SmartEnergy.Library/Measurements/Models/Laadsessie.cs
@@ -2,6 +2,6 @@ namespace SmartEnergy.Library.Measurements.Models;
 public record Laadsessie(DateTime Start,
                           DateTime Stop,
                           TimeSpan Duur,
-                          double Prijs,
-                          double TotaalKWh
+                          double TotaalKWh,
+                          double Prijs
                           );
