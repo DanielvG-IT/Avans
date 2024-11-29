85962114719eb7825f4fb802b60c231ccd926247
Daniël van Ginneken <dcj.vanginneken@student.avans.nl>
2024-11-29 09:50:13 +0100

    Changed File structure again

--- Leerjaar 1/Q2 Robotics Prototyping/Program.cs
+++ Leerjaar 1/Q2 Robotics Prototyping/Program.cs
@@ -6,15 +6,20 @@ namespace MasterRobot
   {
     public static void Main(string[] args)
     {
-      var coolLed = new Led(5);
-      var coolButton = new Button(6);
+      var statusLed = new Led(5);
+      var startButton = new Button(6);
+      // var afstandSensorRight = new Ultrasonic(6);
+      // var afstandSensorLeft = new Ultrasonic(6);
+      // var lineSensorRight = new InfraredReflective(6);
+      // var lineSensorLeft = new InfraredReflective(6);
+      // var colorSensor = new colorSensor();
 
       string storedButtonState = "Unknown";
       bool motorsOn = false;
 
       while (true)
       {
-        var currentButtonStatus = coolButton.GetState();
+        var currentButtonStatus = startButton.GetState();
 
         if (currentButtonStatus != storedButtonState)
         {
@@ -22,17 +27,18 @@ namespace MasterRobot
           {
             if (motorsOn == false)
             {
+              statusLed.SetOn();
               Robot.Motors(150, 150);
               motorsOn = true;
             }
             else if (motorsOn == true)
             {
+              statusLed.SetOff();
               Robot.Motors(0, 0);
               motorsOn = false;
             }
           }
           storedButtonState = currentButtonStatus;
-          Robot.Wait(1000);
         }
       }
     }
