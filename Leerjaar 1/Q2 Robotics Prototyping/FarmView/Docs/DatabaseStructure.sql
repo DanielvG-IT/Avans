ede6e1697f6f63a3108ce2029480bf9e2f87a2cf
Daniël van Ginneken <dcjvanginneken@gmail.com>
2024-12-11 23:51:02 +0100

    Rename tables in SQL to match with code

--- Leerjaar 1/Q2 Robotics Prototyping/FarmView/Docs/DatabaseStructure.sql
+++ Leerjaar 1/Q2 Robotics Prototyping/FarmView/Docs/DatabaseStructure.sql
@@ -1,16 +1,17 @@
 use DB2226789
 go
 
-Create table SensorData(
+Create table SensorHistory(
 	Id int Primary key Identity(1,1) not null,
 	SensorName varchar(255) not null,
 	DataTimestamp  DateTime not null,
 	SensorData varchar(255) not null,
 );
 
-Create table Commands(
+Create table CommandHistory(
 	Id int Primary key Identity(1,1) not null,
+	CommandName varchar(255) not null,
 	DataTimestamp  DateTime not null,
-	Command varchar(255) not null,
+	CommandData varchar(255) not null,
 );
 
