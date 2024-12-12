50dd3e3e0d2bd2eb56c35dc8b588fc638a135385
Daniël van Ginneken <dcjvanginneken@gmail.com>
2024-12-12 18:40:25 +0100

    Implemented AlertSystem functionality

--- Leerjaar 1/Q2 Robotics Prototyping/CropBotics/FarmRobot.cs
+++ Leerjaar 1/Q2 Robotics Prototyping/CropBotics/FarmRobot.cs
@@ -8,22 +8,32 @@ namespace CropBotics;
 
 public class FarmRobot : IInitializable, IUpdatable, IWaitable
 {
+  public FarmRobot()
+  {
+    // Initializing hardware
+    AlertLed = new(AlertLedPin);
+    emergencyStopButton = new(emergencyStopButtonPin);
+
+    // Initializing systems
+    alertSystem = new(this, AlertLed, emergencyStopButton);
+    commsSystem = new();
+    driveSystem = new();
+    obstacleDetectionSystem = new();
+  }
+
   // Local vars for defining hardware 
   const int AlertLedPin = 5;
   const int emergencyStopButtonPin = 6;
 
   // Local vars for including functions
-  private DriveSystem driveSystem = new();
-  private CommsSystem commsSystem = new();
-  private AlertSystem alertSystem = new();
-  private ObstacleDetectionSystem obstacleDetectionSystem = new();
+  private DriveSystem driveSystem;
+  private CommsSystem commsSystem;
+  private AlertSystem alertSystem;
+  private ObstacleDetectionSystem obstacleDetectionSystem;
+  Led AlertLed;
+  Button emergencyStopButton;
 
-  // IInitializing hardware
-  private Button emergencyStopButton = new(emergencyStopButtonPin);
-  private Led AlertLed = new(AlertLedPin);
-
-
-  // TODO: Implement state machine
+  // TODO Implement state machine
   public State CurrentState { get; private set; } = State.INIT;
 
   public void SetState(State State)
@@ -42,7 +52,6 @@ public class FarmRobot : IInitializable, IUpdatable, IWaitable
     Robot.PlayNotes("g>g");
 
     // Initialize systems
-    await alertSystem.Init();
     await commsSystem.Init();
     await driveSystem.Init();
     await obstacleDetectionSystem.Init();
--- Leerjaar 1/Q2 Robotics Prototyping/CropBotics/Functions/AlertSystem.cs
+++ Leerjaar 1/Q2 Robotics Prototyping/CropBotics/Functions/AlertSystem.cs
@@ -1,20 +1,67 @@
+using CropBotics.Models;
 using CropBotics.Interfaces;
 using Avans.StatisticalRobot;
 
 namespace CropBotics.Functions;
 
-public class AlertSystem : IUpdatable, IInitializable
+public class AlertSystem : IUpdatable
 {
+  private FarmRobot _farmRobot;
+  private Led _alertLed;
+  private Button _emergencyButton;
+  private bool _emergencyButtonWasPressed;
+  public bool EmergencyStop { get; set; }
 
-  // TODO 2x tonen via buzzer als emergency stop
 
-  public Task Init()
+  public AlertSystem(FarmRobot farmRobot, Led led, Button button)
   {
-    throw new NotImplementedException();
+    _farmRobot = farmRobot;
+    _emergencyButton = button;
+    _emergencyButtonWasPressed = _emergencyButton.GetState().Equals("Pressed");
+    _alertLed = led;
+  }
+
+
+  public void PlayAlert()
+  {
+    _alertLed.SetOn();
+    Robot.PlayNotes("fd");
+  }
+
+  public void StopAlert()
+  {
+    _alertLed.SetOff();
+    Robot.PlayNotes("f>c");
+  }
+
+  public void EmergencyAlert()
+  {
+    _alertLed.SetOn();
+    Robot.PlayNotes("f");
+    Robot.Wait(100);
+    Robot.PlayNotes("f");
+    Robot.Wait(100);
+    Robot.PlayNotes("f");
   }
 
   public void Update()
   {
-    throw new NotImplementedException();
+    var currentEmergencyButtonState = _emergencyButton.GetState() == "Pressed" ? true : false;
+
+    if (currentEmergencyButtonState && !_emergencyButtonWasPressed)
+    {
+      Console.WriteLine("DEBUG: Emergency stop button pressed");
+      _emergencyButtonWasPressed = true;
+      EmergencyStop = true;
+      EmergencyAlert();
+      _farmRobot.SetState(State.EMERGENCY_STOP);
+    }
+    else if (!currentEmergencyButtonState && _emergencyButtonWasPressed)
+    {
+      _emergencyButtonWasPressed = false;
+      EmergencyStop = false;
+      StopAlert();
+      _farmRobot.SetState(State.READY);
+    }
   }
 }
