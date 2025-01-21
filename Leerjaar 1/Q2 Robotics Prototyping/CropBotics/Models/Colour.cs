d1cb64d8da9d6818872370be09e4827ad38f6369
Daniël van Ginneken <dcj.vanginneken@student.avans.nl>
2025-01-21 11:14:52 +0100

    Expand Colour enum to include more colors and shades

--- Leerjaar 1/Q2 Robotics Prototyping/CropBotics/Models/Colour.cs
+++ Leerjaar 1/Q2 Robotics Prototyping/CropBotics/Models/Colour.cs
@@ -1,12 +1,58 @@
+namespace CropBotics.Models;
 public enum Colour
 {
+  // Basic colors
   Red,
   Green,
   Blue,
   Yellow,
-  White,
+  Orange,
+  Purple,
+  Pink,
   Black,
+  White,
+  Gray,
+  Brown,
   Cyan,
   Magenta,
+
+  // Extended colors
+  Maroon,
+  Olive,
+  Navy,
+  Teal,
+  Lime,
+  Aqua,
+  Fuchsia,
+  Silver,
+  Gold,
+
+  // Pastel shades
+  PastelPink,
+  PastelBlue,
+  PastelGreen,
+  PastelPurple,
+  PastelYellow,
+
+  // Earth tones
+  Sand,
+  Sage,
+  Umber,
+  Rust,
+  Terracotta,
+
+  // Additional colors
+  Peach,
+  Lavender,
+  Coral,
+  Turquoise,
+  Indigo,
+  Mint,
+  Chocolate,
+  Ivory,
+  Charcoal,
+  Beige,
+
+  // Default Return
   Unknown
 }
