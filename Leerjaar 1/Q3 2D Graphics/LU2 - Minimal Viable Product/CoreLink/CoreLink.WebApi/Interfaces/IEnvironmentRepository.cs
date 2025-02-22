67bb8c044046eb18efa4ab0b2ca4100922c8f456
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-02-22 13:28:34 +0100

    Rename repository methods to use async suffix for consistency

--- Leerjaar 1/Q3 2D Graphics/LU2 - Minimal Viable Product/CoreLink/CoreLink.WebApi/Interfaces/IEnvironmentRepository.cs
+++ Leerjaar 1/Q3 2D Graphics/LU2 - Minimal Viable Product/CoreLink/CoreLink.WebApi/Interfaces/IEnvironmentRepository.cs
@@ -12,21 +12,21 @@ namespace CoreLink.WebApi.Interfaces
         /// </summary>
         /// <param name="environment">The environment to create.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        public Task CreateEnvironment(Environment2D environment);
+        public Task CreateEnvironmentAsync(Environment2D environment);
 
         /// <summary>
         /// Retrieves a 2D environment by its unique identifier.
         /// </summary>
         /// <param name="Id">The unique identifier of the environment.</param>
         /// <returns>A task representing the asynchronous operation, containing the environment if found, otherwise null.</returns>
-        public Task<Environment2D?> GetEnvironmentById(Guid Id);
+        public Task<Environment2D?> GetEnvironmentByIdAsync(Guid Id);
 
         /// <summary>
         /// Retrieves a 2D environment by the user's unique identifier.
         /// </summary>
         /// <param name="UserId">The unique identifier of the user.</param>
         /// <returns>A task representing the asynchronous operation, containing the environment if found, otherwise null.</returns>
-        public Task<IEnumerable<Environment2D>> GetEnvironmentsByUserId(string UserId);
+        public Task<IEnumerable<Environment2D>> GetEnvironmentsByUserIdAsync(string UserId);
 
         /// <summary>
         /// Updates a 2D environment by its unique identifier.
@@ -34,13 +34,13 @@ namespace CoreLink.WebApi.Interfaces
         /// <param name="Id">The unique identifier of the environment to update.</param>
         /// <param name="updatedEnvironment">The updated environment data.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        public Task UpdateEnvironmentById(Guid Id, Environment2D updatedEnvironment);
+        public Task UpdateEnvironmentByIdAsync(Guid Id, Environment2D updatedEnvironment);
 
         /// <summary>
         /// Deletes a 2D environment by its unique identifier.
         /// </summary>
         /// <param name="Id">The unique identifier of the environment to delete.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        public Task DeleteEnvironmentById(Guid Id);
+        public Task DeleteEnvironmentByIdAsync(Guid Id);
     }
 }
--- Leerjaar 1/Q3 2D Graphics/LU2 - Minimal Viable Product/CoreLink/CoreLink.WebApi/Repositories/EnvironmentRepository.cs
+++ Leerjaar 1/Q3 2D Graphics/LU2 - Minimal Viable Product/CoreLink/CoreLink.WebApi/Repositories/EnvironmentRepository.cs
@@ -7,32 +7,32 @@ namespace CoreLink.WebApi.Repositories
 {
     public class EnvironmentRepository(string sqlDatabaseConnectionString) : IEnvironmentRepository
     {
-        public async Task CreateEnvironment(Environment2D environment)
+        public async Task CreateEnvironmentAsync(Environment2D environment)
         {
             using var sqlConnection = new SqlConnection(sqlDatabaseConnectionString);
             var rowsAffected = await sqlConnection.ExecuteAsync("INSERT INTO [Environment2D] (id, name, ownerUserId, maxHeight, maxLength) VALUES (@id, @name, @ownerUserId ,@maxHeight, @maxLength)", environment);
         }
 
-        public async Task<Environment2D?> GetEnvironmentById(Guid Id)
+        public async Task<Environment2D?> GetEnvironmentByIdAsync(Guid Id)
         {
             using var sqlConnection = new SqlConnection(sqlDatabaseConnectionString);
             return await sqlConnection.QuerySingleOrDefaultAsync<Environment2D>("SELECT * FROM [Environment2D] WHERE id = @id", new { Id });
         }
 
-        public async Task<IEnumerable<Environment2D>> GetEnvironmentsByUserId(string UserId)
+        public async Task<IEnumerable<Environment2D>> GetEnvironmentsByUserIdAsync(string UserId)
         {
             using var sqlConnection = new SqlConnection(sqlDatabaseConnectionString);
             var userEnvironments = await sqlConnection.QueryAsync<Environment2D>("SELECT * FROM [Environment2D] WHERE ownerUserId = @UserId", new { UserId });
             return userEnvironments;
         }
 
-        public async Task UpdateEnvironmentById(Guid id, Environment2D updatedEnvironment)
+        public async Task UpdateEnvironmentByIdAsync(Guid id, Environment2D updatedEnvironment)
         {
             using var sqlConnection = new SqlConnection(sqlDatabaseConnectionString);
             await sqlConnection.ExecuteAsync("INSERT INTO [Environment2D] (id, name, ownerUserId, maxHeight, maxLength) VALUES (@id, @name, @ownerUserId, @maxHeight, @maxLength)", updatedEnvironment);
         }
 
-        public async Task DeleteEnvironmentById(Guid id)
+        public async Task DeleteEnvironmentByIdAsync(Guid id)
         {
             using var sqlConnection = new SqlConnection(sqlDatabaseConnectionString);
             await sqlConnection.ExecuteAsync("DELETE FROM [Environment2D] WHERE Id = @Id", new { id });
