305aa6833d034dd41a3558372d489aafd85ffa59
Daniël van Ginneken <dcj.vanginneken@student.avans.nl>
2025-02-27 14:35:10 +0100

    Add IsPositionValid method to Environment2D for boundary checks

--- Leerjaar 1/Q3 2D Graphics/LU2 - Minimal Viable Product/CoreLink/CoreLink.WebApi/Models/Environment2D.cs
+++ Leerjaar 1/Q3 2D Graphics/LU2 - Minimal Viable Product/CoreLink/CoreLink.WebApi/Models/Environment2D.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace CoreLink.WebApi.Models;
 
@@ -36,4 +37,16 @@ public class Environment2D
     [Required]
     [Range(10, 100)]
     public int maxHeight { get; set; }
+
+    /// <summary>
+    /// Checks if the given position is within the bounds of the environment.
+    /// </summary>
+    /// <param name="x">The x-coordinate of the position.</param>
+    /// <param name="y">The y-coordinate of the position.</param>
+    /// <returns>True if the position is valid, otherwise false.</returns>
+    /// TODO MAAK UNIT TEST VOOR DIT 1/3
+    public bool IsPositionValid(int x, int y)
+    {
+        return x >= 0 && x <= maxLength && y >= 0 && y <= maxHeight;
+    }
 }
