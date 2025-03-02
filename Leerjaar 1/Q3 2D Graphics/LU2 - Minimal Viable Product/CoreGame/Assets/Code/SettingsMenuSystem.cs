46cb128a8287c949f0e7ed386bd3402709f38a1a
Daniël van Ginneken <dcj.vanginneken@student.avans.nl>
2025-03-02 11:57:41 +0100

    Refactor SettingsMenuSystem to improve environment handling

--- Leerjaar 1/Q3 2D Graphics/LU2 - Minimal Viable Product/CoreGame/Assets/Code/SettingsMenuSystem.cs
+++ Leerjaar 1/Q3 2D Graphics/LU2 - Minimal Viable Product/CoreGame/Assets/Code/SettingsMenuSystem.cs
@@ -17,33 +17,48 @@ public class SettingsMenuSystem : MonoBehaviour
     [Header("Scripts Dependencies")]
     public EnvironmentSystem environmentSystem;
 
+    // Internal
+    private Environment2D currentEnvironment;
+
     void Start()
     {
+        currentEnvironment = GameManager.Instance.SelectedEnvironment;
+        
         settingsMenu.SetActive(false);
+
+        UpdateInputFields();
     }
 
-    public async void GoBackToMainMenu()
+    private void UpdateInputFields()
     {
-        await SceneManager.LoadSceneAsync("MainMenu");
+        if (currentEnvironment == null) 
+            GoBackToMainMenu();
+
+        InputEnvName.text = currentEnvironment.name;
+        InputEnvMaxHeight.text = currentEnvironment.maxHeight.ToString();
+        InputEnvMaxLength.text = currentEnvironment.maxLength.ToString();
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public async void UpdateEnvironment()
     {
         var updatedEnvironment = new Environment2D
         {
-            id = GameManager.Instance.SelectedEnvironment.id,
+            id = currentEnvironment.id,
             name = InputEnvName.text,
             maxHeight = Convert.ToInt32(InputEnvMaxHeight.text),
             maxLength = Convert.ToInt32(InputEnvMaxLength.text),
         };
 
         await environmentSystem.UpdateEnvironment2D(updatedEnvironment);
-        await ReloadEnvironment();
-    }
-
-    public async Task ReloadEnvironment()
-    {
-        await SceneManager.LoadSceneAsync("Environment");
+        
+        currentEnvironment = GameManager.Instance.SelectedEnvironment;
+        
+        // SceneManager.LoadScene("Environment");
     }
 
     public async void DeleteEnvironmentSettingsMenu()
