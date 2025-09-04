9f0eab8549dfa2ba6ee2cffa3d21e180c9547e78
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-09-04 13:56:22 +0200

    feat: add initial Express setup with Pug templating and error handling

--- Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/app.js
+++ Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/app.js
@@ -1,15 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
+import morgan from 'morgan';
 import express from 'express';
 import logger from './util/logger.js';
+import createError from 'http-errors';
+import cookieParser from 'cookie-parser';
 
-const app = express();
+var app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // Routes
 app.get('/', (req, res) => {
@@ -17,7 +27,25 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 
+// 404 Not Found handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
