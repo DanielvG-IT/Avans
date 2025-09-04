d4d3d3a0a868d4b04ae77254d1597e1476f8eea2
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-09-04 13:43:12 +0200

    Add logger utility for production-ready logging and update app.js

--- Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/app.js
+++ Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/app.js
@@ -1,30 +1,11 @@
-import express from 'express';
-import winston from 'winston';
 import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import logger from './util/logger.js';
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 3000;
-
-// Configure production ready logging
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }), // error, fatal, but not other levels
-        new winston.transports.File({ filename: 'combined.log', level: 'info' }), // fatal, error, warn, and info
-    ],
-});
-
-// Log to console if not in production
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(
-        new winston.transports.Console({
-            format: winston.format.simple(),
-        })
-    );
-}
 
 // Middleware
 app.use(express.json());
