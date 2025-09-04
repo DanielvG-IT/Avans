c7891478dd1358f4f458b1207df4cd182bd6fcb9
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-09-04 13:43:12 +0200

    Add logger utility for production-ready logging and update app.js

---
+++ Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/util/logger.js
@@ -0,0 +1,35 @@
+import winston from 'winston';
+const { combine, timestamp, printf, colorize, align } = winston.format;
+
+// Configure production ready logging
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'debug',
+    format: combine(
+        colorize({ all: true }),
+        timestamp({
+            format: 'YYYY-MM-DD hh:mm:ss A',
+        }),
+        align(),
+        printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+    ),
+    defaultMeta: { service: 'expressjs' },
+    transports: [],
+});
+
+// Log to console if not in production
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        })
+    );
+}
+// Log to error files when in production
+else {
+    logger.add(
+        new winston.transports.File({ filename: 'error.log', level: 'error' }), // error, fatal, but not other levels
+        new winston.transports.File({ filename: 'combined.log', level: 'info' }) // fatal, error, warn, and info
+    );
+}
+
+export default logger;
