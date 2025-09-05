6e1dbf1f28ca156dda55f9be73d466fbb79d04f8
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-09-06 00:25:45 +0200

    refactor: rename dbConnection to dbConn and update export statement

--- Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/data/db.js
+++ Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/data/db.js
@@ -1,9 +1,11 @@
 import mysql from 'mysql2';
 
-export const dbConnection = mysql.createPool({
+const dbConn = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
 });
+
+export default dbConn;
