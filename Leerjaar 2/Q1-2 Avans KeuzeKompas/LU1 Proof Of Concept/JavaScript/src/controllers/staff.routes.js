a696d25a34a4b8ba1c6b75824337b5ed452bd66e
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-09-13 18:01:21 +0200

    feat: add dashboard and CRM views with routing for staff section

--- Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/controllers/staff.routes.js
+++ Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/controllers/staff.routes.js
@@ -8,4 +8,12 @@ staffRouter.get('/', requireStaffAuthWeb, (req, res, next) => {
     res.render('staff/overview', { title: 'Staff Overview' });
 });
 
+staffRouter.get('/dashboard', requireStaffAuthWeb, (req, res, next) => {
+    res.render('staff/dashboard', { title: 'Staff Dashboard' });
+});
+
+staffRouter.get('/crm', requireStaffAuthWeb, (req, res, next) => {
+    res.render('staff/crm', { title: 'Customer Relationship Management' });
+});
+
 export default staffRouter;
