304be98b063e1ac34e868b7eb9f54253cc3fc8ed
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-09-19 14:04:42 +0200

    style: format code for consistency and readability

--- Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/data/db.js
+++ Leerjaar 2/Q1-2 Avans KeuzeKompas/LU1 Proof Of Concept/JavaScript/src/data/db.js
@@ -1,30 +1,30 @@
-import mysql from "mysql2";
-import { logger } from "../util/logger.js";
+import mysql from 'mysql2';
+import { logger } from '../util/logger.js';
 
 if (
-  !process.env.DB_HOST ||
-  !process.env.DB_USER ||
-  !process.env.DB_PASS ||
-  !process.env.DB_NAME ||
-  !process.env.DB_PORT
+    !process.env.DB_HOST ||
+    !process.env.DB_USER ||
+    !process.env.DB_PASS ||
+    !process.env.DB_NAME ||
+    !process.env.DB_PORT
 ) {
-  logger.fatal("Missing database configuration");
-  throw new Error("Database configuration is incomplete");
+    logger.fatal('Missing database configuration');
+    throw new Error('Database configuration is incomplete');
 }
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
 });
 
 export const query = (sql, params, callback) => {
-  pool.query(sql, params, (err, results) => {
-    if (err) return callback(err);
-    callback(null, results);
-  });
+    pool.query(sql, params, (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
 };
