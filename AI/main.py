8ba5d190e8ff1220d4ff5dae6ffd2c17a2fc86d5
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-12-15 10:38:47 +0100

    Add predict endpoint to FastAPI application

--- AI/main.py
+++ AI/main.py
@@ -1,10 +1,12 @@
 from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get('/predict')
+def predict(input_data: Union[str, int, float]):
+  return 'Wouter was not here yet!'
