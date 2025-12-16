b8c8367f0c6803e70f3dda2e7008235e04de8dd5
AlirizaSari <AlisSari55@hotmail.com>
2025-12-16 12:31:20 +0100

    feat: implement user retrieval in UserService and UserController with error handling

--- Backend/src/application/ports/user.port.ts
+++ Backend/src/application/ports/user.port.ts
@@ -1,1 +1,6 @@
-export interface IUserService {}
+import { Result } from '@/result';
+import { User } from '@/domain/user/user.model';
+
+export interface IUserService {
+  findById(id: string): Promise<Result<User>>;
+}
