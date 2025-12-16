9d5719018814dba39a7dba806ae636b535b271c8
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-12-16 13:29:32 +0100

    fix: update Prisma configuration to use environment variable for DATABASE_URL and add env variable in CI/CD workflow

--- Backend/prisma.config.ts
+++ Backend/prisma.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'src/infrastructure/database/schema/schema.prisma',
@@ -8,6 +8,8 @@ export default defineConfig({
     seed: 'tsx src/infrastructure/database/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url:
+      process.env.DATABASE_URL ||
+      'postgresql://user:password@localhost:5432/db',
   },
 });
