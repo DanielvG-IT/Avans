3330834f7d8d73c5c1e4ba8ed7ac7140caed4b8d
BaasW <wan.baas@student.avans.nl>
2025-12-17 20:44:51 +0100

    We now seperate input into bits needed for prediciton model. Stitch them together with ". " and also clean any trailing white spaces.

--- AI/controllers/predict_controller.py
+++ AI/controllers/predict_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from models.student_input import StudentInput
+from services.clean_and_stringify import clean_prediction_data
 
 router = APIRouter(
     prefix="/predict",
@@ -8,7 +9,10 @@ router = APIRouter(
 
 @router.post("/")
 def predict(data: StudentInput):
+
+    # First we make one big string of the properties: current study, interests and learning goals. Then we will also remove the trailing white spaces. 
+    cleaned_prediction_data = clean_prediction_data(data)
     return {
-        "message": "Your input data is valid",
-        "data": data
+        "message": "Cleaned data:",
+        "data": cleaned_prediction_data
     }
