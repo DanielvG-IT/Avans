988eae61c37ad5eac9e1f8f334f0ef413a855bbc
JoepieSnels <j.snels@student.avans.nl>
2025-12-18 10:37:22 +0100

    dark mode works now

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -11,37 +11,37 @@ import { ProfilePage } from "./pages/profile";
 import { KeuzehulpPage } from "./pages/keuzehulp.tsx";
 import { LoginPage } from "./pages/auth/login";
 
+// Theme provider
+import { ThemeProvider } from "./context/ThemeContext.tsx";
+
 // Auth provider
 import { AuthProvider } from "./hooks/useAuth";
 import { ProtectedRoute } from "./components/ProtectedRoute";
-
-// Dark mode context
-import { ThemeProvider } from "./context/ThemeContext.tsx";
 
 // Rendering app
 const root = document.getElementById("root")!;
 if (!root) throw new Error("Root element not found!");
 
 createRoot(root).render(
-  <StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            {/* Protected routes */}
-            <Route element={<ProtectedRoute />}>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/modules" element={<ModulesPage />} />
-              <Route path="/modules/:id" element={<ModulePage />} />
-              <Route path="/keuzehulp" element={<KeuzehulpPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-            </Route>
+	<StrictMode>
+		<ThemeProvider>
+			<BrowserRouter>
+				<AuthProvider>
+					<Routes>
+						{/* Protected routes */}
+						<Route element={<ProtectedRoute />}>
+							<Route path="/" element={<HomePage />} />
+							<Route path="/modules" element={<ModulesPage />} />
+							<Route path="/modules/:id" element={<ModulePage />} />
+							<Route path="/keuzehulp" element={<KeuzehulpPage />} />
+							<Route path="/profile" element={<ProfilePage />} />
+						</Route>
 
-            {/* Public routes */}
-            <Route path="/auth/login" element={<LoginPage />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </ThemeProvider>
-  </StrictMode>
+						{/* Public routes */}
+						<Route path="/auth/login" element={<LoginPage />} />
+					</Routes>
+				</AuthProvider>
+			</BrowserRouter>
+		</ThemeProvider>
+	</StrictMode>
 );
